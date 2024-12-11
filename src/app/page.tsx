9001edac16f3638d087d9fc5b18c11f6b815e1c9
lumpen <swk5373@gmail.com>
2024-12-12 00:20:31 +0900

    feat: api test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,14 @@
 const fetchChoice = async () => {
-  const res = await fetch('http://localhost:8080/api/test');
-  const data = await res;
+  const res = await fetch('/api/test');
+  console.log(res);
+  const data = await res.text();
   return data;
 };
 
 export default async function Home() {
-  const choice = await fetchChoice();
-  console.log(choice);
   return (
     <div>
-      {choice}
+      <button onClick={()=>{ fetchChoice() }}>버튼</button>
       {/* <div className="flex flex-col justify-center items-start gap-4">
         <ProgressBar progress="50" />
         <ChoiceLayout title1="오늘 뭐 먹지?" title2="오늘 뭐 먹지?">
