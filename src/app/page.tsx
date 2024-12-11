21e147f75f24ada7d3b78e98adba13b31d55f278
lumpen <swk5373@gmail.com>
2024-12-11 23:13:38 +0900

    feat: api test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,24 @@
-export default function Home() {
-  return <div>home</div>;
+const fetchChoice = async () => {
+  const res = await fetch('/api/test');
+  const data = await res.json();
+  return data;
+};
+
+export default async function Home() {
+  const choice = await fetchChoice();
+  console.log(choice);
+  return (
+    <div>
+      api test
+      {/* <div className="flex flex-col justify-center items-start gap-4">
+        <ProgressBar progress="50" />
+        <ChoiceLayout title1="오늘 뭐 먹지?" title2="오늘 뭐 먹지?">
+          <ChoiceBisection />
+        </ChoiceLayout>
+        <ChoiceLayout title1="오늘 뭐 먹지?" title2="오늘 뭐 먹지?">
+          <ChoiceSection />
+        </ChoiceLayout>
+      </div> */}
+    </div>
+  );
 }
