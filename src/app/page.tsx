e5a17757d1b3121a0177b95f92de1f9028ae07b8
mrpark219 <mrpark219@gmail.com>
2024-12-12 16:24:39 +0900

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
 const fetchChoice = async () => {
-  const res = await fetch('/api/test');
-  console.log(res);
-  const data = await res.text();
-  return data;
+  try {
+    const res = await fetch('/api/test');
+    console.log(res);
+    const data = await res.text();
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 export default async function Home() {
