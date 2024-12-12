fb25f6ce2687a5dbaff808158597f0d88d19647b
5o-hyun <94782559+5o-hyun@users.noreply.github.com>
2024-12-13 02:05:23 +0900

    design: main profile

--- src/app/components/Home/HomeProfile/HomeProfile.tsx
+++ src/app/components/Home/HomeProfile/HomeProfile.tsx
@@ -4,8 +4,14 @@ import Icon from '../../../../../public/icons/icon';
 
 const HomeProfile = () => {
   return (
-    <div className="mx-[24px] p-[28px] bg-[#EDEDED] rounded-[16px] mt-[40px] mb-[58px] flex gap-[12px] items-center cursor-pointer">
-      <Image src="" alt="프로필" width={108} height={108} className="bg-[#F8F8F8] rounded-full" />
+    <div className="mx-[24px] p-[28px] bg-[#FFF3EC] rounded-[16px] mt-[40px] mb-[58px] flex gap-[12px] items-center cursor-pointer">
+      <Image
+        src="../../../../icons/profile.svg"
+        alt="프로필"
+        width={108}
+        height={108}
+        className="bg-[#F8F8F8] rounded-full"
+      />
       <div>
         <p className="font-semibold text-[24px] text-black mb-[4px] mr-[4px]">00님</p>
         <p className="font-semibold text-[20px] text-black flex items-center">
