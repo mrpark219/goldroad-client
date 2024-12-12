2c56863f10eb3098e592fcf9e8098a6794da2a95
mrpark219 <mrpark219@gmail.com>
2024-12-12 16:40:15 +0900

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,6 @@
-const fetchChoice = async () => {
-  try {
-    const res = await fetch('/api/test');
-    console.log(res);
-    const data = await res.text();
-    return data;
-  } catch (error) {
-    console.error(error);
-  }
-};
-
 export default async function Home() {
   return (
     <div>
-      <button onClick={()=>{ fetchChoice() }}>버튼</button>
       {/* <div className="flex flex-col justify-center items-start gap-4">
         <ProgressBar progress="50" />
         <ChoiceLayout title1="오늘 뭐 먹지?" title2="오늘 뭐 먹지?">
