bba9c7c37b00b60bf5c3dc140d812d3004b8a312
5o-hyun <94782559+5o-hyun@users.noreply.github.com>
2024-12-13 03:14:51 +0900

    design: feedback

--- src/app/profile/components/feedback.tsx
+++ src/app/profile/components/feedback.tsx
@@ -1,5 +1,50 @@
+import Image from 'next/image';
+import Icon from '../../../../public/icons/icon';
+import Link from 'next/link';
+
 function Feedback() {
-  return <div>Feedback</div>;
+  return (
+    <>
+      <div className="mt-[34px] mb-[78px]">
+        <p className="font-semibold text-[18px] text-black">내가 받은 피드백</p>
+        <p className="font-medium text-[16px] text-[#666666] mb-[24px]">
+          모임에 참여하고 뱃지를 획득해보세요!
+        </p>
+        <div className="flex gap-[16px] mb-[38px]">
+          <div>
+            <div className="w-[134px] h-[134px] rounded-full bg-[#EEF4FF] mb-[8px] flex justify-center items-center">
+              <Icon name="water" width={58} height={58} />
+            </div>
+            <p className="text-[18px] font-medium text-[#666666] text-center">물+4</p>
+            <p className="text-[16px] font-medium text-[#3755CF] text-center">활발해요!</p>
+          </div>
+          <div>
+            <div className="w-[134px] h-[134px] rounded-full bg-[#EEF4FF] mb-[8px] flex justify-center items-center">
+              <Icon name="sun" width={58} height={58} />
+            </div>
+            <p className="text-[18px] font-medium text-[#666666] text-center">햇빛+4</p>
+            <p className="text-[16px] font-medium text-[#3755CF] text-center">활발해요!</p>
+          </div>
+          <div>
+            <div className="w-[134px] h-[134px] rounded-full bg-[#EEF4FF] mb-[8px] flex justify-center items-center">
+              <Icon name="soy" width={58} height={58} />
+            </div>
+            <p className="text-[18px] font-medium text-[#666666] text-center">거름+4</p>
+            <p className="text-[16px] font-medium text-[#3755CF] text-center">활발해요!</p>
+          </div>
+        </div>
+      </div>
+      <Link href={'/profile/goldroad'}>
+        <Image
+          src="../../../icons/goldorange.svg"
+          alt="보러가기"
+          width={300}
+          height={100}
+          className="cursor-pointer"
+        />
+      </Link>
+    </>
+  );
 }
 
 export default Feedback;
