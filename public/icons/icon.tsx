d30093bfda338081219119227ff64586a0116602
lumpen <swk5373@gmail.com>
2024-12-12 11:28:01 +0900

    남성/여성 선택

---
+++ public/icons/icon.tsx
@@ -0,0 +1,31 @@
+import { useEffect, useRef } from 'react';
+
+const replaceColor = (svgString: string, newColor: string) => {
+  const regex = /fill="[^"]+"/g;
+  const replacement = `fill="${newColor}"`;
+  return svgString.replace(regex, replacement);
+};
+
+interface IconProps {
+  name: string;
+  width: number;
+  height: number;
+  color?: string;
+}
+
+const Icon = ({ name, width, height, color = 'currentColor', ...props }: IconProps) => {
+  const svgRef = useRef<SVGSVGElement>(null);
+
+  useEffect(() => {
+    const svgElement = async () => {
+      const svg = await import(`./${name}.svg`);
+      if (!svgRef.current) return;
+      svgRef.current.innerHTML = replaceColor(svg.default, color);
+    };
+    svgElement();
+  }, [name, color]);
+
+  return <svg width={width} height={height} ref={svgRef} {...props} />;
+};
+
+export default Icon;
--- src/app/components/choice-bisection/bisection-card.tsx
+++ src/app/components/choice-bisection/bisection-card.tsx
@@ -1,5 +1,32 @@
-function BisectionCard() {
-  return <div className="w-[209px] h-[212px] bg-gray-200 rounded-[12px]">BisectionCard</div>;
+import { twMerge } from 'tailwind-merge';
+import Icon from '../../../../public/icons/icon';
+interface BisectionCardProps {
+  iconName: string;
+  text: string;
+  isChoosed: boolean;
+  onClick: (text: string) => void;
+}
+
+function BisectionCard({ iconName, text, isChoosed, onClick }: BisectionCardProps) {
+  const bgColor = isChoosed ? 'bg-primary' : 'bg-gray';
+  const textColor = isChoosed ? 'text-white' : 'text-[#ACACAC]';
+  return (
+    <button
+      className={twMerge(`w-[209px] h-[212px] rounded-[12px] overflow-hidden`)}
+      onClick={() => onClick(text)}
+    >
+      <div
+        className={twMerge(
+          'w-full h-full bg-white flex flex-col justify-center items-center',
+          bgColor,
+          textColor,
+        )}
+      >
+        <Icon name={iconName} width={52} height={52} color={isChoosed ? '#FFFFFF' : '#ACACAC'} />
+        <div>{text}</div>
+      </div>
+    </button>
+  );
 }
 
 export default BisectionCard;
